f5da8fcb995bec8ff46286275f1ca31f9378fa83
yohannes2025 <johannes.m.tekle@gmail.com>
2025-04-19 19:04:32 +0200

    Add SECRET_KEY to env.py

--- env.py
+++ env.py
@@ -6,6 +6,8 @@ os.environ['DATABASE_URL'] = "postgresql://neondb_owner:npg_ZM1uJgwao5Qf@ep-roun
 
 # os.environ['SECRET_KEY'] = "django-insecure-ztits4*i8ygp0%ou9@s)p0f(5-%g56e01$=xt)0!_yho+c7=n4"
 
-os.environ['SECRET_KEY'] = "h,^@p53<bA0IK(tUFI*G"F5Z+*e19iAu^Jtw$W}?nH>^jA(tf6l(r7qRy,D#0)L"
+# os.environ['SECRET_KEY'] = "+I1z*`08P|2XG6H.^QaAPrA"v#0l4CN|p}^#%fHeP9r_Euf7C)`?Pm[T:1c"%6j"
 
 os.environ['DEV'] = '1'
+
+os.environ['SECRET_KEY'] = "+I1z*`08P|2XG6H.^QaAPrA"v#0l4CN|p}^#%fHeP9r_Euf7C)`?Pm[T:1c"%6j"
