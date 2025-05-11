332aeeab7940b82e71de8f70389f0f15653d040c
yohannes2025 <johannes.m.tekle@gmail.com>
2025-05-11 16:34:48 +0200

    Removing git and README files from the frontend folder

---
+++ env.py
@@ -0,0 +1,12 @@
+import os
+
+os.environ['CLOUDINARY_URL'] = "cloudinary://641477698697777:wBLMQwXop1dNYwX9Pg9Dd5jd2As"
+
+os.environ['DATABASE_URL'] = "postgresql://neondb_owner:npg_ZM1uJgwao5Qf@ep-round-violet-a2uhsauy.eu-central-1.aws.neon.tech/slang_darn_stood_367172"
+
+os.environ['SECRET_KEY'] = "=ydT'[+Z/YF^~X#CrRCmIW?rTtASt#8RH+DCzI!#cmf_:PD)fq)L8:,m:k4Md+O"
+
+# os.environ['DEV'] = '1'
+# os.environ['DEBUG'] = '1'
+
+os.environ['CLIENT_ORIGIN'] = "http://127.0.0.1:3000"
